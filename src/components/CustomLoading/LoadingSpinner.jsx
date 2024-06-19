34fa84f570a4c260ccaf30d5bb0521782426220f
huu2k2 <henryng2805@gmail.com>
2024-06-19 23:21:19 +0700

    add

--- src/components/CustomLoading/LoadingSpinner.jsx
+++ src/components/CustomLoading/LoadingSpinner.jsx
@@ -5,7 +5,7 @@ import ClipLoader from "react-spinners/ClipLoader";
 const LoadingSpinner = ({ isLoading, size = 30, color = "#123abc" }) => {
   return (
     isLoading && (
-      <div className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center w-full h-full">
+      <div className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center w-full h-full z-50">
       <div className=" flex items-center justify-center w-20 h-20 bg-gray-400 bg-opacity-50 rounded-lg">
         <div className="  flex items-center justify-center border border-gray-600 rounded-full">
           <ClipLoader size={size} color={color} loading={isLoading} />
