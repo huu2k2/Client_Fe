bb552f9415f2be3b9a8ee25e95f15da615969616
huu2k2 <henryng2805@gmail.com>
2024-06-21 10:28:07 +0700

    handle filter xem them

--- src/components/GridCart/SimilarRoom.jsx
+++ src/components/GridCart/SimilarRoom.jsx
@@ -1,27 +1,37 @@
 import React, { useEffect, useState } from "react";
 import CartRoom from "../Cart_item";
-import { useQueryData } from "@customhooks/FilterCustomHook";
+import {
+  useQueryData,
+  useClickSearchFilter,
+  useQueryFilterData,
+} from "@customhooks/FilterCustomHook";
 import CustomLoading from "../CustomLoading";
-const SimilarRoom = ({ id }) => {
-  const [data, isFetching, isError] = useQueryData();
 
-  if (isFetching) {
-    return (
-      <>
-        <CustomLoading />
-      </>
-    );
-  }
+const SimilarRoom = ({ id }) => {
+  const handleClickSearch = useClickSearchFilter();
+  const [filterData, setFilterData] = useQueryFilterData();
+  const [data, isFetching, isError] = useQueryData();
   const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    if (id !== undefined) {
+      setFilterData({ ...filterData, HouseId: id });
+      handleClickSearch();
+    }
+  }, [id, setFilterData]);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setItems(data?.response || []);
     }, 500);
 
-    // Cleanup the timeout if the component unmounts
     return () => clearTimeout(timer);
   }, [data]);
+
+  if (isFetching) {
+    return <CustomLoading />;
+  }
+ 
   return (
     <div className="grid grid-cols-4 gap-4  gap-y-[56px] relative w-full min-h-[400px] max-h-fit">
       {isError && <CustomLoading />}
--- src/components/ListCartHome/index.jsx
+++ src/components/ListCartHome/index.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import GridCart from '../GridCart';
 import SimilarRoom from '../GridCart/SimilarRoom';
 
 const Index = () => {
   const location = useLocation();
-
+  const { idroom } = useParams();
   // Kiểm tra pathname của location để quyết định khi nào hiển thị GridCart
   const showGridCart = location.pathname === '/';
 
   return (
     <div className='w-[1360px] '>
       {showGridCart && <GridCart id={0}/>}
-      {!showGridCart && <SimilarRoom />}
+      {!showGridCart && <SimilarRoom id={idroom}/>}
     </div>
   );
 };
