6946c10e7b36fd6ccecc1f422ec7794c7b6eb9a9
huu2k2 <henryng2805@gmail.com>
2024-06-30 09:35:01 +0700

    fit color

--- src/modules/OverviewHome/ListRooms/index.jsx
+++ src/modules/OverviewHome/ListRooms/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Body from "./Body";
 import GroupCheckbox from "./GroupCheckbox";
-
+import { AiOutlineArrowRight } from "react-icons/ai";
 const index = () => {
   return (
     <div className="w-full h-fit bg-black flex-col justify-center items-center  flex  ">
@@ -15,7 +15,7 @@ const index = () => {
             <div className="text-white text-3xl font-bold  leading-9">
               Danh sách phòng trống
             </div>
-          <GroupCheckbox/>
+            <GroupCheckbox />
           </div>
 
           <div className="w-80 h-fit px-4 py-3 bg-neutral-50 rounded-2xl flex-col justify-start items-start gap-4 inline-flex">
@@ -29,19 +29,21 @@ const index = () => {
                   <div className="text-black text-base font-medium  leading-normal">
                     Nguyễn văn A
                   </div>
-                  <div className="text-zinc-500 text-xs font-normal  leading-none">
+                  <div className="text-zinc-500 text-xs font-normal  leading-none ">
                     Chủ nhà
                   </div>
                 </div>
               </div>
               <div className="w-6 h-6 relative">
-              <div className="w-6 h-6 left-0 top-0 absolute bg-rose-600 rounded-full" />
-              <div className="w-4 h-4 left-[4px] top-[4px] absolute justify-center items-center inline-flex">
-                <div className="w-4 h-4 relative"></div>
+                <div className="w-6 h-6 left-0 top-0 absolute bg-rose-600 rounded-full flex justify-center items-center">
+                  <AiOutlineArrowRight className="text-white"/>
+                </div>
+                <div className="w-4 h-4 left-[4px] top-[4px] absolute justify-center items-center inline-flex">
+                  <div className="w-4 h-4 relative"></div>
+                </div>
               </div>
             </div>
-            </div>
-           
+
             <div className="justify-center items-center gap-2 inline-flex">
               <div className="px-2 flex-col justify-start items-start inline-flex">
                 <div className="w-[120px] text-zinc-500 text-xs font-normal  leading-none">
@@ -63,7 +65,7 @@ const index = () => {
           </div>
         </div>
       </div>
- 
+
       <Body />
     </div>
   );
