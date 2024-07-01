961240da3292a49af1a184adaf34014aa3a88d36
huu2k2 <henryng2805@gmail.com>
2024-07-01 22:22:20 +0700

    fix giao dieenj noi that co ban trong mo gioi

--- src/modules/Home/HomePage_Detail/Category/BasicInterior.jsx
+++ src/modules/Home/HomePage_Detail/Category/BasicInterior.jsx
@@ -2,22 +2,23 @@ import { BsCheckCircle } from "react-icons/bs";
 import { BsDoorOpenFill } from "react-icons/bs";
 import { useDataServices } from "@customhooks";
 import BasicInteriorSkeleton from "./CategorySkeleton/BasicInteriorSkeleton";
+import CheckCircle from '../../../../assets/CheckCircle.png'
 const BasicInterior = () => {
   const [furnitureInserts] = useDataServices();
 
   return (
     <>
-    <div className="w-[552px] h-[136px] gap-5 nthd_flex_col_between">
+    <div className="w-[552px] h-fit gap-5 nthd_flex_col_between">
       <div className="w-[145px] h-6  nthd_text_medium_base nthd_flex_between">
         <BsDoorOpenFill />
         <span> Nội thất cơ bản</span>
       </div>
 
-      <div className="grid grid-cols-2  w-full h-[92px]">
+      <div className="grid grid-cols-2  w-full h-fit gap-4">
         {furnitureInserts ? (
           furnitureInserts.map((i, index) => (
-            <div className="nthd_basicInterior_item" key={index}>
-              <BsCheckCircle className="bg-green-600 text-white rounded-lg" />
+            <div className="nthd_basicInterior_item " key={index}>
+              <img src={CheckCircle}   />
               <h5 className="nthd_text_medium_sm_text  w-[219px] h-5 flex justify-between">
                 {" "}
                 {i?.furnitureName}{" "}
