a689e30be0449e7bf9a6e681039c1bdabf5a832e
huu2k2 <henryng2805@gmail.com>
2024-07-03 21:23:56 +0700

    update

--- src/modules/OverviewHome/Policies/Deposit.jsx
+++ src/modules/OverviewHome/Policies/Deposit.jsx
@@ -27,7 +27,7 @@ const Deposit = ({data}) => {
               Giữ phòng tối đa
             </div>
             <div className="w-[838px] text-gray-900 text-sm font-normal  leading-tight">
-              7 ngày
+              {data.numberOfDaysKeepRoom} ngày
             </div>
           </div>
         </div>
