0799d58839bb4b9107cf3f0b9524ca4b03c56f0b
huu77 <thuu28052002@gmail.com>
2024-08-10 15:37:44 +0700

    fix: load lại phòng tương tự

--- src/components/GridCart/index.jsx
+++ src/components/GridCart/index.jsx
@@ -1,4 +1,10 @@
-import React, { useEffect, useState, useMemo, useCallback, useTransition } from "react";
+import React, {
+  useEffect,
+  useState,
+  useMemo,
+  useCallback,
+  useTransition,
+} from "react";
 import CartRoom from "../Cart_item";
 import {
   useQueryData,
@@ -15,10 +21,13 @@ const Index = ({ id, money, address, category, faveritedata, option }) => {
   const { data: datadistrict } = useGetDistrictsQuery();
   const handleClickSearch = useClickSearchFilter();
   const location = useLocation();
-  
+
   const [isPending, startTransition] = useTransition();
   const [data, { isLoading }] = useQueryData();
   const [items, setItems] = useState([]);
+  
+  // Kiểm soát lần load đầu tiên
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
 
   const findDistrictId = useCallback((address, districts) => {
     const district = districts?.results.find(
@@ -27,33 +36,47 @@ const Index = ({ id, money, address, category, faveritedata, option }) => {
     return district ? district.district_id : null;
   }, []);
 
-  const query = useMemo(() => ({
-    houseId: id || null,
-    districtId: findDistrictId(address, datadistrict) || null,
-    price: Number(money) === 0 ?null :{
-      min: Number(money) === 0 ? null : Math.max(Number(money) - 500000, 0),
-      max: Number(money) === 0 ? null : Number(money) + 500000,
-    },
-    categories: category ? [category] : null,
-  }), [id, money, category, datadistrict, findDistrictId]);
+  const query = useMemo(
+    () => ({
+      houseId: id,
+      districtId: findDistrictId(address, datadistrict),
+      price:
+        Number(money) === 0
+          ? null
+          : {
+              min:
+                Number(money) === 0
+                  ? null
+                  : Math.max(Number(money) - 500000, 0),
+              max: Number(money) === 0 ? null : Number(money) + 500000,
+            },
+      categories: category ? [category] : null,
+    }),
+    [id, money, category, datadistrict, findDistrictId]
+  );
 
   useEffect(() => {
-    startTransition(() => {
-      setFilterData((prevData) => ({ ...prevData, ...query }));
-    });
+    if (isFirstLoad) {
+      setIsFirstLoad(false);
+      // Thực hiện gọi API với query lần đầu tiên
+      setFilterData({ ...filterData, ...query });
+      handleClickSearch(); // Gọi API ngay lập tức lần đầu tiên
+    } else {
+      // Chỉ cập nhật filterData sau khi query hoàn tất thay đổi
+      setFilterData({ ...filterData, ...query });
+    }
   }, [query]);
 
   useEffect(() => {
     if (
-      (location.pathname === "/similarRooms" &&
+      (location.pathname.includes("similarRooms") &&
         money !== null &&
         address !== null) ||
       id !== null
     ) {
       handleClickSearch();
     }
-    
-  }, [ location.pathname, money, address, id]);
+  }, [location.pathname, money, address, id]);
 
   useEffect(() => {
     setIsLoading(isLoading);
@@ -72,10 +95,12 @@ const Index = ({ id, money, address, category, faveritedata, option }) => {
     });
   }, [option, data]);
 
-  if (isPending&&!data?.response?.length) {
+  if (isPending && !data?.response?.length) {
     return (
       <div className="w-full h-full flex justify-center items-center">
-        <p className="text-rose-500">{isPending ? "Đang tải dữ liệu..." : "Không tìm thấy phòng tương tự!"}</p>
+        <p className="text-rose-500">
+          {isPending ? "Đang tải dữ liệu..." : "Không tìm thấy phòng tương tự!"}
+        </p>
       </div>
     );
   }
